71320b596f2796e33bfbdb03f0dda092a7a9c392
carlosMiguelz <carlos.miguel.r7@gmail.com>
2019-05-03 15:40:50 -0300

    guilherme aqui

---
+++ app/Http/Controllers/EquipamentoController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Equipamento;
+
+class EquipamentoController extends Controller
+{   
+
+    public function __construct(){
+        $this -> middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $equipamentos = Equipamento::all();
+
+        return view('equipamentos.index',compact('equipamentos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('equipamentos.cadastro');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $equipamento = $request ->all();
+        Equipamento::create($equipamento);
+        return redirect()->route('equipamentos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $equipamento = Equipamento::find($id);
+        $equipamento->delete();
+        return redirect('equipamentos.index');
+    }
+}
---
+++ database/migrations/2019_05_03_174212_create_equipamentos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEquipamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipamentos', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name');
+            $table->Interge('tipo')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('tipo')->references('id')->on('tipos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipamentos');
+    }
+}
